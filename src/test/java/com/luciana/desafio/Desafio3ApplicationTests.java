4a90571744610f5f686bb10d32e14599237a6c61
Luciana <lt.shimoyama@gmail.com>
2024-07-08 12:00:54 -0300

    Projeto criado

---
+++ src/test/java/com/luciana/desafio/Desafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciana.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
